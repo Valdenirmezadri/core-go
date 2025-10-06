50cc65ed0b8c5069b4182b34d929f6947654bbe2
Valdenir Mezadri <juniordarkeden@gmail.com>
2025-10-06 19:19:09 -0300

    removeaccents text

---
+++ text/helper.go
@@ -0,0 +1,15 @@
+package text
+
+import (
+	"unicode"
+
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+)
+
+func RemoveAccents(s string) string {
+	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+	output, _, _ := transform.String(t, s)
+	return output
+}
