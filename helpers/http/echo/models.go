80ea01f7c413f704dfa0d96ed84c575215aa84c9
Valdenir Mezadri <juniordarkeden@gmail.com>
2026-03-06 19:24:01 -0300

    Unmarshall results

--- helpers/http/echo/models.go
+++ helpers/http/echo/models.go
@@ -1,10 +1,24 @@
 package helperecho
 
+import "encoding/json"
+
 type Result struct {
 	Message string `json:"message,omitempty"`
 	Data    any    `json:"data,omitempty"`
 }
 
+func (Result) Unmarshall(b []byte) (v Result, err error) {
+	return _unmarshall[Result](b)
+}
+
 type Err struct {
 	Errors []string `json:"errors"`
+}
+
+func (Err) Unmarshall(b []byte) (v Err, err error) {
+	return _unmarshall[Err](b)
+}
+
+func _unmarshall[T Result | Err](b []byte) (v T, err error) {
+	return v, json.Unmarshal(b, &v)
 }
